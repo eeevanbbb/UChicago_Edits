52f159967b622f4ccfbc70ad3f8af8e43fca034f
Evan Bernstein <eeevanbbb@me.com>
2017-01-28 17:51:28 -0600

    fix datetime issue

--- listen.py
+++ listen.py
@@ -1,8 +1,8 @@
 import websocket
 import json
-from datetime import datetime
+import datetime
 
-REPEAT_EDIT_MAX_TIME = 60 * 5 # 5 Minutes
+REPEAT_EDIT_MAX_TIME_MINUTES = 5
 
 # {"action": "edit", "change_size": -70, "flags": null, "hashtags": [], "is_anon": false, "is_bot": false, "is_minor": false, "is_new": false, "is_unpatrolled": false, "mentions": [], "ns": "Main", "page_title": "List of Vikings characters", "parent_rev_id": "762313049", "rev_id": "762311834", "summary": null, "url": "https://en.wikipedia.org/w/index.php?diff=762313049&oldid=762311834", "user": "TheVampire"}
 class WikipediaEdit():
@@ -10,13 +10,13 @@ class WikipediaEdit():
 		message_dict = json.loads(message)
 		for key in message_dict:
 			setattr(self, key, message_dict[key])
-		self.date = datetime.now()
+		self.date = datetime.datetime.now()
 
 	def __repr__(self):
 		return "Edit of page %s" % self.page_title.encode('utf-8')
 
 	def is_repeat_of_edit(self, edit):
-		return edit is not None and edit.user == self.user and edit.page_title == self.page_title and abs(self.date - edit.date) < REPEAT_EDIT_MAX_TIME
+		return edit is not None and edit.user == self.user and edit.page_title == self.page_title and abs(self.date - edit.date) < datetime.timedelta(minutes=REPEAT_EDIT_MAX_TIME_MINUTES) 
 
 
 class WikipediaListener():
